c314b5bc6b3699166e8bebc89a6198980ad96312
Pedro M. Dominguez <84252067+xtcedro@users.noreply.github.com>
2025-04-16 19:28:51 -0500

    Update createAdminUser.js

--- utils/createAdminUser.js
+++ utils/createAdminUser.js
@@ -9,14 +9,56 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-function ask(question) {
-  return new Promise(resolve => rl.question(question, resolve));
+function ask(question, { hideInput = false } = {}) {
+  return new Promise(resolve => {
+    if (!hideInput) {
+      rl.question(question, resolve);
+    } else {
+      const stdin = process.stdin;
+      const onDataHandler = char => {
+        char = char + "";
+        switch (char) {
+          case "\n":
+          case "\r":
+          case "\u0004":
+            stdin.pause();
+            break;
+          default:
+            process.stdout.clearLine(0);
+            process.stdout.cursorTo(0);
+            process.stdout.write(question + "*".repeat(password.length));
+            password += char;
+            break;
+        }
+      };
+
+      let password = "";
+      process.stdout.write(question);
+      stdin.on("data", onDataHandler);
+      stdin.setRawMode(true);
+      stdin.resume();
+
+      stdin.once("end", () => resolve(password));
+      stdin.once("keypress", () => {
+        stdin.setRawMode(false);
+        stdin.removeListener("data", onDataHandler);
+        resolve(password);
+      });
+
+      rl.once("line", () => {
+        stdin.setRawMode(false);
+        stdin.removeListener("data", onDataHandler);
+        resolve(password);
+      });
+    }
+  });
 }
 
 async function createAdmin() {
   try {
     const username = await ask("Enter admin username: ");
-    const password = await ask("Enter admin password: ");
+    const password = await ask("Enter admin password: ", { hideInput: true });
+    console.log(); // for newline
     const hash = await bcrypt.hash(password, 12);
 
     const db = await mysql.createConnection({
